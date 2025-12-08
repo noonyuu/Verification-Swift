c07521e3e3a749077e57411bf1383ca793a1e892
noonyuu <noon3m.op.sh@gmail.com>
2025-12-08 23:25:54 +0900

    feat: genericパターン

--- Verification/Views/Generic.swift
+++ Verification/Views/Generic.swift
@@ -2,6 +2,7 @@ import SwiftUI
 
 struct GenericView: View {
     @State private var noGenericValue: Bool = false
+    @State private var genericValue: Bool = false
     
     var body: some View {
         // ジェネリック不使用
@@ -10,15 +11,29 @@ struct GenericView: View {
             Button {
                 noGenericValue = noGeneric(1, 2)
             } label: {
-                Text("比較")
+                Text("ジェネリック不使用:比較")
             }
             .padding()
             Text(noGenericValue ? "True" : "False")
+        }
+        HStack(spacing: 0) {
+            Button {
+                genericValue = generic("1", "1")
+            } label: {
+                Text("ジェネリック使用:比較")
+            }
+            .padding()
+            Text(genericValue ? "True" : "False")
         }
     }
 }
 
 func noGeneric(_ x: Int, _ y: Int) -> Bool {
+    x == y
+}
+
+// <T: Equatable> → Equatableに準拠するすべての型
+func generic<T: Equatable>(_ x: T, _ y: T) -> Bool {
     x == y
 }
 
