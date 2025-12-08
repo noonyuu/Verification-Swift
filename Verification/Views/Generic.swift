9927974f8a2ee57dfd9ec372e38b861ac61f9b0a
noonyuu <noon3m.op.sh@gmail.com>
2025-12-08 23:27:46 +0900

    feat: genericパターン

--- Verification/Views/Generic.swift
+++ Verification/Views/Generic.swift
@@ -3,6 +3,7 @@ import SwiftUI
 struct GenericView: View {
     @State private var noGenericValue: Bool = false
     @State private var genericValue: Bool = false
+    @State private var mixGenericResult: String = ""
     
     var body: some View {
         // ジェネリック不使用
@@ -25,6 +26,16 @@ struct GenericView: View {
             .padding()
             Text(genericValue ? "True" : "False")
         }
+        HStack(spacing: 0) {
+            Button {
+                let result = otherOne(1, "2", 1)
+                mixGenericResult = "結果: \(result.0) : \(type(of: result.0)), \(result.1) : \(type(of: result.1)), \(String(describing: result.2)): \(type(of: result.2))"
+            } label: {
+                Text("ジェネリック混合:実行")
+            }
+            .padding()
+            Text(mixGenericResult)
+        }
     }
 }
 
@@ -35,6 +46,16 @@ func noGeneric(_ x: Int, _ y: Int) -> Bool {
 // <T: Equatable> → Equatableに準拠するすべての型
 func generic<T: Equatable>(_ x: T, _ y: T) -> Bool {
     x == y
+}
+
+// 型引数として宣言された型は通常の型と同じように扱える
+// ジェネリック関数の戻り値の型としても使用できる
+func otherOne<T, U>(_ x: T, _ y: U, _ z: T) -> (T, U, T?) {
+    let a: T = x        // 型アノテーション
+    let b = y           // 型推論
+    let c = z           // 型のキャスト
+    
+    return (a, b, c)
 }
 
 #Preview {
